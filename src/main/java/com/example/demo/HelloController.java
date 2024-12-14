7ac685a028e9f97c4f0dcadf0844099f6573b1f3
usmanshafique7 <s7.usman@gmail.com>
2024-12-15 03:48:13 +0500

    test

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// This tells Spring Boot that this class is a Controller
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    // This is a simple GET API
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World! Welcome to your first API!";
+    }
+}
+
